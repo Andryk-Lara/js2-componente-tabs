9df7c33a7fbbb0a300380daea346754cf6c6cfb7
Andryk Lara <lara.cordero@gmail.com>
2020-09-16 09:19:15 -0600

    Agrego ACTIVIDAD 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function accion(){
+/*function accion(){
     event.preventDefault();//Que el link no cambie
     const id = event.currentTarget.getAttribute('href').substring(1);
     console.log(id);
@@ -30,4 +30,32 @@ tabs[0].classList.add('js-active');
 for(let i = 0; i < tabs.length; i++){
     tabs[i].addEventListener('click', accion);
 }
+*/
+const tabs = document.querySelectorAll('.tabs-tablist-item');
 
+const infos = document.querySelectorAll('.tabs-content-item');
+
+for(let i = 0; i < infos.length; i++){
+    infos[i].classList.add('js-content-hidden');
+}
+
+infos[0].classList.remove('js-content-hidden');
+tabs[0].classList.add('js-active');
+
+for(let i = 0; i < tabs.length; i++){
+    tabs[i].addEventListener('click', (event) =>{
+        event.preventDefault();//Que el link no cambie
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
+        for (let i = 0; i < infos.length; i++){
+            infos[i].classList.add('js-content-hidden');
+            if (infos[i].getAttribute('id') == id) {
+            contenido[i].classList.remove('js-content-hidden');
+            }
+        }
+        for(let i = 0; i < tabs.length; i++){
+            tabs[i].classList.remove('js-active');
+        }
+        event.currentTarget.classList.add('js-active');
+    });
+}
